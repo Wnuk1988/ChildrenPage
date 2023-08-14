740220db412ebe915b1e67fa56e8f6c70183b0de
Михаил <Wnuk1988@mail.ru>
2023-08-14 23:47:10 +0300

    Commit, update favorites table!

--- src/main/java/com/tms/models/DescriptionFile.java
+++ src/main/java/com/tms/models/DescriptionFile.java
@@ -1,5 +1,6 @@
 package com.tms.models;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -7,13 +8,15 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CascadeType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,7 +45,9 @@ public class DescriptionFile {
     @Column(name = "genre")
     @Enumerated(EnumType.STRING)
     private Genre genre;
-    @Column(name = "favorites")
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "description_file_id")
-    private List<Favorites> favorites = new ArrayList<>();
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name="L_user_file",
+            joinColumns=  @JoinColumn(name="description_file_id", referencedColumnName="id"),
+            inverseJoinColumns= @JoinColumn(name="user_info_id", referencedColumnName="id") )
+    private List<UserInfo> userInfoList = new ArrayList<>();
 }
--- src/main/java/com/tms/models/UserInfo.java
+++ src/main/java/com/tms/models/UserInfo.java
@@ -1,5 +1,6 @@
 package com.tms.models;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -7,13 +8,15 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CascadeType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,7 +44,9 @@ public class UserInfo {
     @Column(name = "user_role")
     @Enumerated(EnumType.STRING)
     private Role role;
-    @Column(name = "favorites")
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user_info_id")
-    private List<Favorites> favorites = new ArrayList<>();
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name="L_user_file",
+            joinColumns=  @JoinColumn(name="user_info_id", referencedColumnName="id"),
+            inverseJoinColumns= @JoinColumn(name="description_file_id", referencedColumnName="id") )
+    private List<DescriptionFile> favoritesFile = new ArrayList<>();
 }
